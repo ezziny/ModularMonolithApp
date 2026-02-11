6b6a1bb6a5a0a9c88aadcf94c5fd32e904720572
ezz el deen ahmed <ezzinynone@gmail.com>
2026-02-11 03:17:07 +0200

    definitely didn't forget about the gitignore...

--- src/Modules/Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
+++ src/Modules/Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7c8f25b372813ac2ce227ba7993f3a8ff52bb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c892e5f91a55fb879a8ec5b74ebfb5d13d12e26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
